d14709bc1ac8074c7f5fed43057191198fa77af3
Sergio Diaz <sadiazcaballero@gmail.com>
2024-04-19 10:37:07 +0200

    final Entry

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", () => {
   /************  HTML ELEMENTS  ************/
   // View divs
@@ -59,12 +60,39 @@ document.addEventListener("DOMContentLoaded", () => {
 
   /************  TIMER  ************/
 
-  let timer;
+  /////NEW/////
+
+  let timer = quiz.timeRemaining; // 120 sec
+
+  function makeMinAndSec(time) {
+    const minutes = Math.floor(time / 60)
+      .toString()
+      .padStart(2, "0");
+    const seconds = (time % 60).toString().padStart(2, "0");
+  
+    return `${minutes}:${seconds}`;
+  }
+
+  function timeRemaining() {
+    timer--;
+
+    if (timer === 0) {
+      showResults();
+      clearInterval(timerOut);
+    }
+
+    timeRemainingContainer.innerText = makeMinAndSec(timer);
+  }
+
+  let timerOut = setInterval(timeRemaining, 1_000);
+
+  /////NEW//////
 
 
   /************  EVENT LISTENERS  ************/
 
   nextButton.addEventListener("click", nextButtonHandler);
+  restartButton.addEventListener("click", restartButtonHandler)
 
 
 
@@ -98,19 +126,20 @@ document.addEventListener("DOMContentLoaded", () => {
     //
     // 1. Show the question
     // Update the inner text of the question container element and show the question text
-
+    questionContainer.innerText = question.text;
     
     // 2. Update the green progress bar
     // Update the green progress bar (div#progressBar) width so that it shows the percentage of questions answered
-    
-    progressBar.style.width = `65%`; // This value is hardcoded as a placeholder
+    let questionPercent = ((quiz.currentQuestionIndex + 1) / quiz.questions.length) * 100;
+
+    progressBar.style.width =  `${questionPercent}%`; // This value is hardcoded as a placeholder
 
 
 
     // 3. Update the question count text 
     // Update the question count (div#questionCount) show the current question out of total questions
     
-    questionCount.innerText = `Question 1 of 10`; //  This value is hardcoded as a placeholder
+    questionCount.innerText = `Question ${quiz.currentQuestionIndex + 1} of ${quiz.questions.length}`;
 
 
     
@@ -127,7 +156,20 @@ document.addEventListener("DOMContentLoaded", () => {
       // Hint 2: You can use the `element.type`, `element.name`, and `element.value` properties to set the type, name, and value of an element.
       // Hint 3: You can use the `element.appendChild()` method to append an element to the choices container.
       // Hint 4: You can use the `element.innerText` property to set the inner text of an element.
+    question.choices.forEach((choice) => {
+      const newRadio = document.createElement("input");
+      const radLabel = document.createElement("label");
 
+      radLabel.innerHTML = choice;
+
+      newRadio.setAttribute("type", "radio");
+      newRadio.setAttribute("name", "choice");
+      newRadio.setAttribute("value", choice);
+
+      choiceContainer.appendChild(newRadio);
+      choiceContainer.appendChild(radLabel);
+      choiceContainer.appendChild(document.createElement("br"));
+    })
   }
 
 
@@ -140,14 +182,34 @@ document.addEventListener("DOMContentLoaded", () => {
     // YOUR CODE HERE:
     //
     // 1. Get all the choice elements. You can use the `document.querySelectorAll()` method.
-
+      const choiceElements = document.querySelectorAll("input[name=choice]");
 
     // 2. Loop through all the choice elements and check which one is selected
       // Hint: Radio input elements have a property `.checked` (e.g., `element.checked`).
       //  When a radio input gets selected the `.checked` property will be set to true.
       //  You can use check which choice was selected by checking if the `.checked` property is true.
-
       
+      
+      choiceElements.forEach((choice => {
+        if(choice.checked) {
+          selectedAnswer = choice.value;
+        }
+      }));
+
+      if (selectedAnswer) {
+        const question = quiz.getQuestion();
+        if (selectedAnswer !== question.answer) {
+          quiz.moveToNextQuestion();
+          showQuestion();
+          return;
+        }
+        quiz.checkAnswer (selectedAnswer);
+        quiz.moveToNextQuestion();
+        showQuestion();
+      }
+
+    
+
     // 3. If an answer is selected (`selectedAnswer`), check if it is correct and move to the next question
       // Check if selected answer is correct by calling the quiz method `checkAnswer()` with the selected answer.
       // Move to the next question by calling the quiz method `moveToNextQuestion()`.
@@ -163,12 +225,37 @@ document.addEventListener("DOMContentLoaded", () => {
     //
     // 1. Hide the quiz view (div#quizView)
     quizView.style.display = "none";
-
+    quizView.classList.add("hidden");
+    
     // 2. Show the end view (div#endView)
+    let endView = document.querySelector('#endView')
     endView.style.display = "flex";
     
     // 3. Update the result container (div#result) inner text to show the number of correct answers out of total questions
-    resultContainer.innerText = `You scored 1 out of 1 correct answers!`; // This value is hardcoded as a placeholder
+    resultContainer.innerText = `You scored ${quiz.correctAnswers} out of ${questions.length} correct answers!`; // This value is hardcoded as a placeholder
   }
   
 });
+
+
+let resetButton = document.querySelector('#restartButton');
+
+resetButton.addEventListener('click', (e) => {
+  endView.style.display = "none";
+
+  quizView.style.display = "flex";
+
+  quiz.currentQuestionIndex = 0;
+  quiz.correctAnswers = 0;
+
+  quiz.shuffleQuestions();
+
+  
+
+  timer = quiz.timeRemaining;
+
+  timeRemainingContainer.innerText = makeMinAndSec(timer);
+
+  timerOut = setInterval(timeRemaining, 1000);
+  showQuestion();
+})
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,54 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
     
-    // 3. moveToNextQuestion()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
 
-    // 4. shuffleQuestions()
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
+    
+    shuffleQuestions() {
+        for (let i = this.questions.length -1; i > 0 ; i--) {
+            let j  = Math.floor(Math.random()*(i + 1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+        }
+    }
 
-    // 5. checkAnswer(answer)
+    checkAnswer(answer) {
+        const currentQuestion = this.getQuestion();
+        if (answer === currentQuestion.answer) {
+            this.correctAnswers++;
+        }
+    }
 
-    // 6. hasEnded()
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false;
+        } else if (this.currentQuestionIndex === this.questions.length) {
+        return true;
+    }
+}
+    filterQuestionsByDifficulty(difficulty) {
+        this.questions = this.questions.filter((element) => {
+            if (difficulty > 0 && difficulty <= 3){
+                return element.difficulty === difficulty;
+            } else {
+                return this.questions;
+            }
+        });
+        }
+
+    averageDifficulty() {
+        const totalDifficulty = this.questions.reduce((acc, question) => acc + question.difficulty, 0);
+        return totalDifficulty / this.questions.length
+    }
+    
 }
